f8524c1d7ef0f9386ad867c5feae450166a47e28
Cristian Meira <cristian.meira15@gmail.com>
2026-05-11 14:07:14 -0300

    Created jobService to create  a job

---
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/service/JobService.java
@@ -0,0 +1,17 @@
+package com.br.cristian.gestao_vagas.modules.service;
+
+import com.br.cristian.gestao_vagas.modules.models.Job;
+import com.br.cristian.gestao_vagas.modules.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public Job createCandidate(Job jobEntity) {
+        return this.jobRepository.save(jobEntity);
+    }
+}
