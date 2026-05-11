a21e456b2e285dc2f20861493afec9909ee0face
Cristian Meira <cristian.meira15@gmail.com>
2026-05-11 14:23:25 -0300

    Updated candidade service to encode the password

--- gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/service/CandidateService.java
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/service/CandidateService.java
@@ -3,6 +3,7 @@ import com.br.cristian.gestao_vagas.modules.exception.UserFoundException;
 import com.br.cristian.gestao_vagas.modules.models.Candidate;
 import com.br.cristian.gestao_vagas.modules.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,19 @@ public class CandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Candidate createCandidate(Candidate candidateEntity){
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user)->{
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
