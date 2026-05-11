722010faeacf318ffb1494a9a97057aa50a9fd26
Cristian Meira <cristian.meira15@gmail.com>
2026-05-11 14:17:18 -0300

    Created the service to create a new company

--- gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/service/CompanyService.java
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/service/CompanyService.java
@@ -1,4 +1,30 @@
 package com.br.cristian.gestao_vagas.modules.service;
 
+import com.br.cristian.gestao_vagas.modules.exception.UserFoundException;
+import com.br.cristian.gestao_vagas.modules.models.Company;
+import com.br.cristian.gestao_vagas.modules.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CompanyService {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Company createCompany(Company companyEntity) {
+        this.companyRepository
+                .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+                .ifPresent((user)->{
+                    throw new UserFoundException();
+                });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
+
+        return this.companyRepository.save(companyEntity);
+    }
 }
